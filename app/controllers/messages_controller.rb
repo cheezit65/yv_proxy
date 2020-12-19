6a26342de3f09988ece341036a85a70e08bc3685
Kimberley Foster <cheezit65@gmail.com>
2020-12-18 19:07:35 -0500

    hi

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,18 +3,18 @@ class MessagesController < InheritedResources::Base
   def create
     if verify_recaptcha
       EmailPage.email_friend(params[:user1][:email1],params[:user2][:email2], params[:id], params[:yearling], params[:hip]).deliver
-      # flash[:notice] = "Email Sent to your friend at #{params[:user2][:email2]} "
-      redirect_to :back
+      flash[:notice] = "Email Sent to your friend at #{params[:user2][:email2]} "
+      redirect_back
     else
       flash[:notice] = "Captcha checkbox not checked. Try again."
-      redirect_to :back
+      redirect_back
     end
   end
 
   def new
     @sale = Sale.find(params[:sale])
     ClientReportMailer.email_client(params[:email1], params[:farm], params[:sale]).deliver
-    redirect_to :back
+    redirect_back
   end
 
 
