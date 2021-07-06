ac85818842fc1da4918802da2652a65c3b2a3f33
Kimberley Foster <cheezit65@gmail.com>
2021-07-06 15:40:52 -0400

    7.6.2021

--- app/controllers/bounces_controller.rb
+++ app/controllers/bounces_controller.rb
@@ -41,15 +41,15 @@ end
 
 
   def new
-# # To get the code below to work, make sure that the yaml and aws.rb files are either deleted or have
-# # the correct access_key_id/secret_access_key/bucket or this will fail.
-# 
-  # #initiate the client
-  # s3 = Aws::S3::Client.new({
-      # region:            'us-east-2',
-      # access_key_id:     'AKIAI6FXAV2E76ELVK5Q',
-      # secret_access_key: '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'
-  # })
+# To get the code below to work, make sure that the yaml and aws.rb files are either deleted or have
+# the correct access_key_id/secret_access_key/bucket or this will fail.
+
+  #initiate the client
+  s3 = Aws::S3::Client.new({
+      region:            'us-east-2',
+      access_key_id:     'AKIAI6FXAV2E76ELVK5Q',
+      secret_access_key: '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'
+  })
   # #Get the object
     # Aws.use_bundled_cert!
   # resp = s3.get_object({ bucket:'yv-output2', key: params[:filename] }, target: params[:filename])
