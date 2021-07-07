7667a1325f5e1cd3cfd5a81da86514382fea9442
Kimberley Foster <cheezit65@gmail.com>
2021-07-07 19:21:13 -0400

    7.7.2021

--- app/controllers/bounces_controller.rb
+++ app/controllers/bounces_controller.rb
@@ -52,7 +52,7 @@ end
   })
   #Get the object
     Aws.use_bundled_cert!
-    buckety='yv-input/' + :filename
+    buckety='yv-output2/' + :filename
   resp = s3.get_object({ bucket:buckety, key: params[:filename] }, target: params[:filename])
 
  # #resp.body
