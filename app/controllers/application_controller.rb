f46e52d68f392b6ff32e215fe86cb3b8495db110
Kimberley Foster <cheezit65@gmail.com>
2021-07-13 06:44:00 -0400

    7.13.2021

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
     
-   ## Added on 7.13.2021 #######################
-    before_action :block_ip_addresses
-
-  protected
-
-  def block_ip_addresses
-    head :unauthorized if current_ip_address[0..2] == "10."
-  end
-
-  def current_ip_address
-    request.env['HTTP_X_REAL_IP'] || request.env['REMOTE_ADDR']
-  end
-#################################################
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
