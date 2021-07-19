86ceb1efccb2570ae88b0a8b9adde0b6583ec3a8
Kimberley Foster <cheezit65@gmail.com>
2021-07-19 06:46:38 -0400

    7.19.2021

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -4,4 +4,14 @@ class Rack::Attack
   Rack::Attack.blocklist('block_local_network')  do|req|
     RANGE.include?(IPAddr.new(req.ip).to_i)
   end
+  LOGGER = Logger.new('log/rack-attack.log')
+ActiveSupport::Notifications.subscribe('rack.attack') do |_name, _start, _finish, _request_id, req|
+  req = req[:request]
+  msg = [req.env['rack.attack.match_type'], req.ip, req.request_method, req.fullpath, ('"' + req.user_agent.to_s + '"')].join(' ')
+  if [:throttle, :blocklist].include? req.env['rack.attack.match_type']
+    LOGGER.error(msg)
+  else
+    LOGGER.info(msg)
+  end
+end
 end
