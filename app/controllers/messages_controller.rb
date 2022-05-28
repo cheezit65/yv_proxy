ae6e84876292b6a5502f72ea71bd36f305ad0400
Kimberley Foster <cheezit65@gmail.com>
2022-05-28 10:16:49 -0400

    2022 E

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,16 @@
 class MessagesController < InheritedResources::Base 
    
   def create
+    
     if verify_recaptcha
-      EmailPage.email_friend(params[:user1][:email1],params[:user2][:email2], params[:id], params[:yearling], params[:hip]).deliver
-      flash[:notice] = "Email Sent to your friend at #{params[:user2][:email2]} "
-      redirect_back(fallback_location: root_path)
-
+      if params[:user1]
+        EmailPage.email_friend(params[:user1][:email1],params[:user2][:email2], params[:id], params[:yearling], params[:hip]).deliver
+        flash[:notice] = "Email Sent to your friend at #{params[:user2][:email2]} "
+        redirect_back(fallback_location: root_path)
+      else
+        redirect_to dwnlds_url
+      end
+    
     else
       flash[:notice] = "Captcha checkbox not checked. Try again."
       redirect_back(fallback_location: root_path)
