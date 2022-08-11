d5a50d82eba39f55572464b19f57d76096554564
Kimberley Foster <cheezit65@gmail.com>
2022-08-11 15:25:18 -0400

    8.11.2022

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :dwnlds
   resources :bulk_adds
   resources :linkers
   resources :viewers
@@ -10,8 +9,6 @@ Rails.application.routes.draw do
   get 'uploads/new'
   get 'uploads/create'
   get 'uploads/index'
-  get 'vectors/new'
-  get 'vectors/index'
   post 'horses/new'
   resources :bulk_uploads
   resources :searches
