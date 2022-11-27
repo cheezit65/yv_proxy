293f0ab02a444a6fbe2aa85a3a69288dd759a2e6
Kimberley Foster <cheezit65@gmail.com>
2022-11-26 20:00:22 -0500

    first render push

--- db/migrate/20160403211055_create_farms.rb
+++
@@ -1,8 +0,0 @@
-class CreateFarms < ActiveRecord::Migration[6.0]
-  def change
-    create_table :farms do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20160407153014_add_farmcode_to_farm.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldsToFarm < ActiveRecord::Migration
-  def change
-    add_column :farms, :FarmCode, :string
-  end
-end
