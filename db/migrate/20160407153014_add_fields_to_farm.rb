5ad04f7f8479df5bd5d541b3b1734d64fb46635d
Kimberley Foster <cheezit65@gmail.com>
2022-11-26 19:43:21 -0500

    first render push

---
+++ db/migrate/20160407153014_add_fields_to_farm.rb
@@ -0,0 +1,12 @@
+class AddFieldsToFarm < ActiveRecord::Migration
+  def change
+    add_column :farms, :name, :string
+    add_column :farms, :address, :string
+    add_column :farms, :city, :string
+    add_column :farms, :state, :string
+    add_column :farms, :zipcode, :string
+    add_column :farms, :phonenumber, :string
+    add_column :farms, :website, :string
+    add_column :farms, :email, :string
+  end
+end
