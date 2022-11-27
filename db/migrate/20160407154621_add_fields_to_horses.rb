d060fd2b68e981258e799b5af7a599ef49d56253
Kimberley Foster <cheezit65@gmail.com>
2022-11-26 22:23:02 -0500

    first render push

---
+++ db/migrate/20160407154621_add_fields_to_horses.rb
@@ -0,0 +1,11 @@
+class AddFieldsToSales < ActiveRecord::Migration[6.0]
+  def change
+    add_column :horses, :sire, :string
+    add_column :horses, :farm, :string
+    add_column :horses, :farm2, :string
+    add_column :horses, :fb_count, :string
+    add_column :horses, :tw_count, :string
+    add_column :horses, :g_count, :string
+    add_column :horses, :e_count, :string    
+  end
+end
