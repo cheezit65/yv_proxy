857094e9d8427fa1e412341c39133b966586f6e4
Kimberley Foster <cheezit65@gmail.com>
2022-11-26 22:34:04 -0500

    first render push

--- db/migrate/20165400154621_add_fields_to_sales.rb
+++
@@ -1,9 +0,0 @@
-class AddFieldsToSales < ActiveRecord::Migration[6.0]
-  def change
-    add_column :sales, :Location, :string
-    add_column :sales, :website, :string
-    add_column :sales, :StartDate, :string
-    add_column :sales, :SaleCode, :string
-    add_column :sales, :EndDate, :string
-  end
-end
