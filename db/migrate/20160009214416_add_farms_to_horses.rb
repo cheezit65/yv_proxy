f21994a5106ec88612e99ffaacd8377360eaca67
Kimberley Foster <cheezit65@gmail.com>
2022-12-05 06:13:20 -0500

    second render push with updated horse table

---
+++ db/migrate/20160009214416_add_farms_to_horses.rb
@@ -0,0 +1,13 @@
+class CreateHorses < ActiveRecord::Migration[6.0]
+  def change
+    t.string :farm
+    t.string :farm2
+    t.string :BulkUploadVideoName
+    t.integer :fb_count
+    t.integer :tw_count
+    t.integer :e_count
+    t.integer :g_count
+    t.string :video
+    t.timestamps null: false
+end
+  end
