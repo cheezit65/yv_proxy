96a730f8e134cc43e4c208cdd2bf6b8aa7807b47
Kimberley <fostershomeemail@yahoo.com>
2023-06-26 08:55:54 -0400

    trying recaptcha again

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = '6Lefu88mAAAAAIe2LXTfTX4UXTE2lkW2wXtTUJoG'
-  config.secret_key = '6Lefu88mAAAAAGgHQ3bmHxLFOnxxIBN4192EGJnC'
+  config.site_key = '6LdNw88mAAAAAPJYSkc6yXrvzII0t6ISQ22Qr1Cq'
+  config.secret_key = '6LdNw88mAAAAAHH10RObr-JVwYRe18Td4XR36nW4'
 end
