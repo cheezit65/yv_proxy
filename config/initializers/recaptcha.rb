f075400a17758a8916fb758e81f38fe821b6bb15
Kimberley <fostershomeemail@yahoo.com>
2023-06-26 08:50:44 -0400

    trying recaptcha again

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = '6LeoyvQUAAAAAN_4lTxNeCIuBvyFnHJ7pbryVBMc'
-  config.secret_key = '6LeoyvQUAAAAADJz1tAY2FxmYDpbbfdZtQjUPa8y'
+  config.site_key = '6Lefu88mAAAAAIe2LXTfTX4UXTE2lkW2wXtTUJoG'
+  config.secret_key = '6Lefu88mAAAAAGgHQ3bmHxLFOnxxIBN4192EGJnC'
 end
