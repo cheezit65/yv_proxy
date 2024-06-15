67a58592c26ec85761d0e38b98840a2e28551957
Kimberley <fostershomeemail@yahoo.com>
2024-06-15 13:28:47 -0400

    render push5

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'csv'
 require 'rails/all'
-
+config.serve_static_files
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
