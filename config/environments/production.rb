3cbd60bfe36185c4aa29d188d0c5eb43b8f0a7b9
Kimberley <fostershomeemail@yahoo.com>
2024-06-15 13:49:44 -0400

    proxy final push1 for yearling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-config.action_mailer.default_url_options = { :host => 'las247.com' }
+config.action_mailer.default_url_options = { :host => 'yearlingvideos.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.smtp_settings = {
