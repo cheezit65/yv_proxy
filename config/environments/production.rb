fa25e5b50d02a836cb5d7b310b18cdccc418e482
Kimberley <fostershomeemail@yahoo.com>
2024-06-15 12:55:29 -0400

    render push5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = false
   
-  config.hosts << /las247\.com/
-
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
@@ -82,7 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-config.action_mailer.default_url_options = { :host => 'yearlingvideos.com' }
+config.action_mailer.default_url_options = { :host => 'las247.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.smtp_settings = {
